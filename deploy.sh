cce831941672ebcd963138b49a2b57aead396071
lixin <1045909037@qq.com>
2018-07-02 13:21:28 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-NAMESPACE="monitoring"
-
-sed -i "s/\$NAMESPACE/$NAMESPACE/g" ./configs/prometheus/prometheus.yaml
-sed -i "s/\$NAMESPACE/$NAMESPACE/g" ./manifests/grafana/import-dashboards/job.yaml
-sed -i "s/\$NAMESPACE/$NAMESPACE/g" ./manifests/prometheus/rbac.yaml
-
 ./build.sh
+NAMESPACE="monitoring"
+sed -i "s/\$NAMESPACE/$NAMESPACE/g" ./manifests-all.yaml
 
 kubectl create namespace $NAMESPACE
 kubectl -n $NAMESPACE apply -f ./manifests-all.yaml
